b103c8500227c6baff6dc649482546c10042556d
arvind <achurox1121@gmail.com>
2025-02-18 23:29:35 +0530

    fix: responsiveness changes for petcare section

--- src/components/Petcare/petcare.tsx
+++ src/components/Petcare/petcare.tsx
@@ -6,19 +6,30 @@ import Collar from '../../../public/petcare/dog-collar.png'
 import Base from '../../../public/petcare/base.png'
 function Petcare() {
   return (
-    <div className="relative bg-foreground w-full min-h-screen text-center">
-      <h1 className='font-borela text-black text-6xl mt-16'>THE FUTURE <span className='text-gray-600'>OF</span> PET CARE</h1>
+    <div className="flex flex-col items-center md:items-stretch md:relative bg-foreground w-full min-h-screen text-center">
+      <h1 className='font-borela text-black p-4 text-3xl md:text-6xl mt-0 md:mt-16'>THE FUTURE <span className='text-gray-600'>OF</span> PET CARE</h1>
+      <div className='md:hidden text-md p-4 md:text-xl text-left flex flex-col items-center'>
+              <Image
+                src={CollarFeatures}
+                height={150}
+                width={150}
+                alt='dog icon'
+                className='object-contain mb-4 '
+              ></Image>
+              <p className='text-gray-600'>Our smart collars offer you peace of mind with real-time tracking 
+                and health insights for your pets.</p>
+      </div>
       <Image
         src={Collar}
         height={700}
         width={700}
         alt='center image'
-        className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-[1]'
+        className='md:absolute md:top-1/2  md:mt-0 mt-4 md:left-1/2 md:transform md:-translate-x-1/2 md:-translate-y-1/2 z-[1]'
       />
-      <div className='relative flex flex-row justify-between min-h-96 text-black mt-24 z-[2]'>
+      <div className='relative flex md:flex-row md:justify-between flex-col items-center min-h-96 text-black mt-24 z-[2]'>
         <div className='flex justify-start'>
-          <div className='flex flex-row items-end w-[30rem] text-right p-4'>
-            <div className='text-xl text-left'>
+          <div className='flex flex-row items-end md:w-[30rem] text-right p-4'>
+            <div className='text-xl text-left md:block hidden'>
               <Image
                 src={CollarFeatures}
                 height={150}
@@ -31,18 +42,25 @@ function Petcare() {
             </div>
           </div>
         </div>
-        <div className='flex justify-end'>
-          <div className='flex flex-row items-start w-[30rem] text-right p-4'>
+        <div className='flex justify-center w-full p-4 items-center md:justify-end'>
+          <div className='flex flex-col items-center bg-white md:bg-transparent rounded-xl m-2 md:flex-row md:items-start md:w-[30rem] md:text-right p-4'>
+            <Image
+              src={DogIcon}
+              height={100}
+              width={100}
+              alt='dog icon'
+              className='object-contain block md:hidden'
+            ></Image>
             <div className='text-xl'>
               <p className='font-bold mb-2'>Stay Linked. Stay Informed.</p>
-              <p className='text-gray-600'>Elevate confidence in pet care with MyPerro Smart Collars</p>
+              <p className='text-gray-600 md:text-xl text-lg'>Elevate confidence in pet care with MyPerro Smart Collars</p>
             </div>
             <Image
               src={DogIcon}
               height={100}
               width={100}
               alt='dog icon'
-              className='object-contain'
+              className='object-contain hidden md:block'
             ></Image>
           </div>
         </div>
@@ -50,7 +68,7 @@ function Petcare() {
       <Image
         src={Base}
         alt='bottom left image'
-        className='absolute bottom-0 h-[300] w-3/4 left-0 z-[0]'
+        className=' hidden md:absolute bottom-0 h-[300] w-3/4 left-0 z-[0]'
       />
     </div>
   )
