ebb8112a7e38f838fb50bf429a3d5399f2bbf44b
arvind <achurox1121@gmail.com>
2025-02-19 13:49:09 +0530

    fix: change breaking pts for petcare + img positions

--- src/components/Petcare/petcare.tsx
+++ src/components/Petcare/petcare.tsx
@@ -6,9 +6,9 @@ import Collar from '../../../public/petcare/dog-collar.png'
 import Base from '../../../public/petcare/base.png'
 function Petcare() {
   return (
-    <div className="flex flex-col items-center md:items-stretch md:relative bg-foreground w-full min-h-screen text-center">
-      <h1 className='font-borela text-black p-4 text-3xl md:text-6xl mt-0 md:mt-16'>THE FUTURE <span className='text-gray-600'>OF</span> PET CARE</h1>
-      <div className='md:hidden text-md p-4 md:text-xl text-left flex flex-col items-center'>
+    <div className="flex flex-col items-center lg:items-stretch lg:relative bg-foreground w-full min-h-screen text-center">
+      <h1 className='font-borela text-black p-4 text-3xl lg:text-6xl mt-0 lg:mt-16'>THE FUTURE <span className='text-gray-600'>OF</span> PET CARE</h1>
+      <div className='lg:hidden text-lg p-4 lg:text-xl text-left flex flex-col items-center'>
               <Image
                 src={CollarFeatures}
                 height={150}
@@ -24,12 +24,12 @@ function Petcare() {
         height={700}
         width={700}
         alt='center image'
-        className='md:absolute md:top-1/2  md:mt-0 mt-4 md:left-1/2 md:transform md:-translate-x-1/2 md:-translate-y-1/2 z-[1]'
+        className='lg:absolute lg:top-[58%]  lg:mt-0 mt-4 lg:left-1/2 lg:transform lg:-translate-x-1/2 lg:-translate-y-1/2 z-[1]'
       />
-      <div className='relative flex md:flex-row md:justify-between flex-col items-center min-h-96 text-black mt-24 z-[2]'>
+      <div className='relative flex lg:flex-row lg:justify-between flex-col lg:items-stretch items-center min-h-96 text-black lg:mt-24 z-[2]'>
         <div className='flex justify-start'>
-          <div className='flex flex-row items-end md:w-[30rem] text-right p-4'>
-            <div className='text-xl text-left md:block hidden'>
+          <div className='flex flex-row items-end lg:w-[30rem] text-right p-4'>
+            <div className='text-xl text-left lg:block hidden'>
               <Image
                 src={CollarFeatures}
                 height={150}
@@ -42,25 +42,25 @@ function Petcare() {
             </div>
           </div>
         </div>
-        <div className='flex justify-center w-full p-4 items-center md:justify-end'>
-          <div className='flex flex-col items-center bg-white md:bg-transparent rounded-xl m-2 md:flex-row md:items-start md:w-[30rem] md:text-right p-4'>
+        <div className='flex justify-center w-full p-4 items-center lg:items-stretch lg:justify-end'>
+          <div className='flex flex-col items-center bg-white lg:bg-transparent rounded-xl m-2 lg:flex-row lg:items-start lg:w-[30rem] lg:text-right p-4'>
             <Image
               src={DogIcon}
               height={100}
               width={100}
               alt='dog icon'
-              className='object-contain block md:hidden'
+              className='object-contain block lg:hidden'
             ></Image>
             <div className='text-xl'>
               <p className='font-bold mb-2'>Stay Linked. Stay Informed.</p>
-              <p className='text-gray-600 md:text-xl text-lg'>Elevate confidence in pet care with MyPerro Smart Collars</p>
+              <p className='text-gray-600 lg:text-xl text-lg'>Elevate confidence in pet care with MyPerro Smart Collars</p>
             </div>
             <Image
               src={DogIcon}
               height={100}
               width={100}
               alt='dog icon'
-              className='object-contain hidden md:block'
+              className='object-contain hidden lg:block'
             ></Image>
           </div>
         </div>
@@ -68,7 +68,7 @@ function Petcare() {
       <Image
         src={Base}
         alt='bottom left image'
-        className=' hidden md:absolute bottom-0 h-[300] w-3/4 left-0 z-[0]'
+        className=' hidden lg:block lg:absolute bottom-0 h-[300] w-3/4 left-0 z-[0]'
       />
     </div>
   )
