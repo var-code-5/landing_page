4c5abb10a7d825d2a39b6a7d61f9cb017e28bfd9
arvind <achurox1121@gmail.com>
2025-02-23 11:02:47 +0530

    feat: add newsletter page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,11 @@ import Petcare from "@/components/Petcare/petcare";
 import Features from "@/components/Features/Features";
 import HeroCollar from "@/components/HeroCollar";
 import Companies from "@/components/Companies/Companies";
+import Newsletter from "@/components/Newsletter/Newsletter";
 export default function Home() {
   return (
     <>
-      <Companies />
+      <Newsletter />
     </>
   );
 }
